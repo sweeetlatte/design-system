be2a9693f973c1a3287562ebc2fb48aa1c227490
sweetcapucino <thuylinh271055@gmail.com>
2022-05-27 16:27:11 +0700

    docs: update config colors

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -25,12 +25,16 @@ const theme = makeTheme({
         "100%",
     ],
     colors: {
+        // prefix "onExample": dùng cho màu text trên element có background-color là example
+        // primary
         primary: "#F3A446",
+        lightPrimary: "#eab679",
+        darkPrimary: "#DA933F",
         onPrimary: "black",
-        primaryVariant: "#DA933F",
-        primaryMuted: "#eab679",
+        // background - secondary
         background: "#1D1E1E",
-        text: "white", //onBackground
+        text: "white",
+        // error
         error: "#F83B3B",
         onError: "white",
     },
@@ -45,7 +49,7 @@ const theme = makeTheme({
             borderRadius: 3,
             cursor: "pointer",
             "&:hover": {
-                bg: "primaryVariant",
+                bg: "darkPrimary",
             },
         },
         outlined: {
@@ -60,8 +64,8 @@ const theme = makeTheme({
             borderRadius: 3,
             cursor: "pointer",
             "&:hover": {
-                color: "primaryVariant",
-                borderColor: "primaryVariant",
+                color: "darkPrimary",
+                borderColor: "darkPrimary",
             },
         },
         elevated: {
@@ -75,13 +79,13 @@ const theme = makeTheme({
             cursor: "pointer",
             boxShadow: "0 8px 27px -3px rgba(243,164,70,0.8)",
             "&:hover": {
-                bg: "primaryVariant",
+                bg: "darkPrimary",
                 boxShadow: "0 8px 27px -3px rgba(218,147,63,0.6)",
             },
         },
         disabled: {
             color: "#333",
-            bg: "primaryMuted",
+            bg: "lightPrimary",
             border: "none",
             px: 4,
             py: 2,
@@ -98,7 +102,7 @@ const theme = makeTheme({
             borderRadius: 3,
             cursor: "pointer",
             "&:hover": {
-                color: "primaryVariant",
+                color: "darkPrimary",
             },
         },
     },
